eecda404782f36f4638bf82dfc4dde5d76688c4e
Kwiatkowska-W <wikikwit2004@gmail.com>
2021-12-22 14:52:44 +0100

    nie uruchamia sie

---
+++ app/src/main/java/xml/example/camera/MainActivity.kt
@@ -0,0 +1,54 @@
+package xml.example.camera
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import androidx.core.app.ActivityCompat
+import android.Manifest
+import android.graphics.Bitmap
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    var btn = findViewById<Button>(R.id.button)
+    var img = findViewById<ImageView>(R.id.imageViev)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn.isEnabled = false
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!=PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions( this, arrayOf(Manifest.permission.CAMERA), 111)
+        }
+        else
+            btn.isEnabled = true
+
+        btn.setOnClickListener{
+            var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(i, 101)
+
+
+    }
+}
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode==101){
+            var mojObrazek: Bitmap?
+            mojObrazek = data?.getParcelableExtra<Bitmap>("data")
+            img.setImageBitmap(mojObrazek)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode==111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            btn.isEnabled = true
+        }
+    }
+}
